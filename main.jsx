c8df6a7e841a05eaac17804e180efea6a50bf925
lukemacdonald1990-netizen <luke.macdonald1990@googlemail.com>
2026-06-04 13:20:34 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WorldCup2026 from './WorldCup2026.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WorldCup2026 />
+  </React.StrictMode>,
+)
